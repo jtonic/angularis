3f0b8d97338145a459526cf9a968b434ad41793b
jtonic <antonel.pazargic@gmail.com>
2015-11-04 06:11:30 +0100

    Use a nice inherits method for objects inheritance

--- spec/jstgp/inheritance_spec.js
+++ spec/jstgp/inheritance_spec.js
@@ -6,17 +6,30 @@
 
 describe('inheritance', function() {
 
+    let Mammal = function(name) {
+        this.name = name
+    }
+    Mammal.prototype.get_name = function() {
+        return this.name
+    }
+    Mammal.prototype.says = function() {
+        return this.saying || ''
+    }
+
+    Function.prototype.method = function(name, func) {
+        if(!this.prototype[name]) {
+            this.prototype[name] = func
+            return this
+        }
+    }
+
+    Function.method('inherits', function(Parent){
+        this.prototype = new Parent()
+        return this // this doesn't work in order to use cascade
+    })
+
     it('pseudoclassical', function() {
-        let Mammal = function(name) {
-            this.name = name
-        }
-        Mammal.prototype.get_name = function() {
-            return this.name
-        }
-        Mammal.prototype.says = function() {
-            return this.saying || ''
-        }
-        var dog = new Mammal('Milli')
+        let dog = new Mammal('Milli')
         expect(dog.get_name()).toBe('Milli')
         expect(dog.says()).toBeFalsy()
         var Cat = function(name) {
@@ -33,5 +46,16 @@ describe('inheritance', function() {
             return `Cat says my name is ${this.name}!!!`
         }
         expect(cat.get_name()).toBe('Cat says my name is Fluffy!!!')
+        let Mouse = function(name) {
+            this.name = name
+            this.saying = 'chits'
+        }
+        .inherits(Mammal)
+        Mouse.prototype.says = function() {
+            return `The mouse says my name is ${this.name}`
+        }
+        let mouse = new Mouse('Jerry')
+        expect(mouse.get_name()).toBe('Jerry')
+        expect(mouse.says()).toBe('The mouse says my name is Jerry')
     })
 })
